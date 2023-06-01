a21422fecae3e5363859dd6922bacc7b1173ba21
tan <tan60332021@163.com>
2023-06-01 16:47:44 +0800

    push_to_github.bat脚本自动提交

--- add_commit_push_to_github.bat
+++ add_commit_push_to_github.bat
@@ -2,8 +2,7 @@
 chcp 65001 > nul
 echo 正在将本地仓库推送到GitHub...
 setlocal
-set TIMESTAMP=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
-set COMMIT_MSG="push_to_github.bat脚本自动提交 %TIMESTAMP%"
+set COMMIT_MSG="push_to_github.bat脚本自动提交"
 git add .
 IF ERRORLEVEL 1 (
     echo 添加文件失败！请检查是否有文件需要添加。
@@ -16,7 +15,7 @@ IF ERRORLEVEL 1 (
     pause
     exit /b
 )
-git push git@github.com:van66991/java-notes.git master
+git push note master
 IF ERRORLEVEL 1 (
     echo 推送失败！请检查远程库是否存在，以及是否有推送权限。
     pause
