087dfcf6cc5c4ef2e1c133ec86a7123c776cf91a
tan <tan60332021@163.com>
2023-06-01 16:56:36 +0800

    push_to_github.bat脚本自动提交 16:56:35.75 周四 2023/06/01

--- push_to_github.bat
+++ push_to_github.bat
@@ -12,18 +12,24 @@ set date=%DATE%
 echo 当前日期：%date%
 
 set COMMIT_MSG="push_to_github.bat脚本自动提交 %time% %date%"
+
+echo 开始添加暂存区...
 git add .
 IF ERRORLEVEL 1 (
     echo 添加文件失败！请检查是否有文件需要添加。
     pause
     exit /b
 )
+
+echo 开始提交本地库...
 git commit -m %COMMIT_MSG%
 IF ERRORLEVEL 1 (
     echo 提交失败！请检查是否有更改需要提交。
     pause
     exit /b
 )
+
+echo 正在将本地仓库推送到GitHub...
 git push note master
 IF ERRORLEVEL 1 (
     echo 推送失败！请检查远程库是否存在，以及是否有推送权限。
