359d793bf43ed3187632f04230fc9aa1f16038df
tan <tan60332021@163.com>
2023-06-01 16:40:50 +0800

    push_to_github.bat脚本自动提交 周四 22306164049

--- commit_push_to_github.bat
+++ commit_push_to_github.bat
@@ -4,12 +4,6 @@ echo 正在将本地仓库推送到GitHub...
 setlocal
 set TIMESTAMP=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
 set COMMIT_MSG="push_to_github.bat脚本自动提交 %TIMESTAMP%"
-git add .
-IF ERRORLEVEL 1 (
-    echo 添加文件失败！请检查是否有文件需要添加。
-    pause
-    exit /b
-)
 git commit -m %COMMIT_MSG%
 IF ERRORLEVEL 1 (
     echo 提交失败！请检查是否有更改需要提交。
